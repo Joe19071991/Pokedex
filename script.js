477431c3673d67de0f181e7367518e4a5d56b2fb
Joe19071991 <lauranove66@gmail.com>
2025-02-27 13:58:45 +0100

    etape 1 ok

---
+++ script.js
@@ -0,0 +1,8 @@
+// async function getData() {
+//   const response = await fetch(
+//     "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json"
+//   );
+//   const data = await response.json();
+//   console.log(data);
+// }
+// getData();
